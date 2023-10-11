3146b96b56d3257a066afa8608ece0d557c8cc68
NedcloarBR <nedcloar1@hotmail.com>
2023-10-11 16:32:07 -0300

    Update install.sh
    Now the script has an Interactive Menu
    script version 0.1.0
    not recommended to use in this moment

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,64 +1,218 @@
 #!/bin/bash
 
-# Update System
-sudo apt update  -y && sudo apt upgrade -y
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root(sudo)"
+  exit 1
+fi
 
-# # Install required modules
-sudo apt install build-essential dkms gcc wget make zip unzip git curl zsh -y
+update="Update System - (Recommended)"
+requirements="Install Requirements - (Essential)"
+zsh="Install ZSH + Plugins & Theme as default Shell - (Recommended)"
+docker="Install Docker - (Optional)"
+nodejs="Install NVM (Node Version Manager) - (Optional)"
+java="Install SDKMan! (Java Version Manager) - (Optional)"
+rust="Install Rust + Cargo Plugins - (Optional)"
+python="Install PyEnv (Python Version Manager) - (Optional)"
+options=("$update" "$requirements" "$zsh" "$docker" "$nodejs" "$java" "$rust" "$python")
+selected=()
+current=0
 
-# # Set zsh as default bash and install Oh My ZSH
-chsh -s /bin/zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+createMenu() {
+  tput clear
+  echo "Select the options you want:"
 
-# # Clone NedcloarBR's config files
-git clone https://github.com/NedcloarBR/dotfiles
-mv ./dotfiles/Linux/.zshrc ./dotfiles/Linux/.p10k.zsh $HOME
-sudo mv ./dotfiles/Linux/wsl.conf ../../etc
-rm -rf ./dotfiles
+  for i in "${!options[@]}"; do
+    if [[ $i -eq $current ]]; then
+      if [[ " ${selected[@]} " =~ " ${options[i]} " ]]; then
+        echo "-> [x] ${options[i]}"
+      else
+        echo "-> [ ] ${options[i]}"
+      fi
+    else
+      if [[ " ${selected[@]} " =~ " ${options[i]} " ]]; then
+        echo "   [x] ${options[i]}"
+      else
+        echo "   [ ] ${options[i]}"
+      fi
+    fi
+  done
+}
 
-# # Clone Oh My ZSH theme and plugins
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+runCommands() {
+  for option in "${selected[@]}"; do
+    case "$option" in
+    "$update")
+      Iupdate
+      ;;
+    "$zsh")
+      Izsh
+      ;;
+    "$docker" | "$nodejs" | "$java" | "$rust" | "$python")
+      if [[ ! " ${selected[@]} " =~ " $requirements " ]]; then
+        echo "Esta Opção só roda com requirements"
+      else
+        Irequirements
+        case "$option" in
+        "$docker")
+          Idocker
+          ;;
+        "$nodejs")
+          Invm
+          ;;
+        "$java")
+          Ijava
+          ;;
+        "$rust")
+          Irust
+          ;;
+        esac
+      fi
+      ;;
+    esac
+  done
+}
 
-# # Install and configure NodeJS with nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+main() {
+  while true; do
+    createMenu
+    read -r -sn1 key
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    if [[ "$key" == "q" && $current -eq 0 ]]; then
+      continue
+    fi
 
-nvm install Gallium # v16.19.0
-nvm install Hydrogen # v18.14.0
-nvm install node # v19.6.0 (latest in 16/02/2023 - 20:00)  DD/MM/YYYY - UTC-3
+    case "$key" in
+    "p")
+      if [[ " ${selected[@]} " =~ " ${options[current]} " ]]; then
+        selected=("${selected[@]/${options[current]}/}")
+      else
+        selected+=("${options[current]}")
+      fi
+      ;;
+    'q')
+      tput clear
+      runCommands
+      echo "Pressione qualquer tecla para sair..."
+      read -n 1 -s -r
+      exit 0
+      ;;
+    'A')
+      if [[ $current -gt 0 ]]; then
+        ((current--))
+      fi
+      ;;
+    'B')
+      if [[ $current -lt $((${#options[@]} - 1)) ]]; then
+        ((current++))
+      fi
+      ;;
+    esac
 
-# Install and configure Docker
-curl -o- -fsSL https://get.docker.com | bash
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
-sudo chmod g+rwx "$HOME/.docker" -R
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+    show_menu
+  done
+}
 
-# Install and configure Java with SDKMan
-curl -o- -s -fsSL "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 19.0.2-oracle
+Iupdate() {
+  sudo apt update -y && sudo apt upgrade -y &&
+    echo -e "System was Updated!"
 
-# Install and configure Rust and Cargo plugins
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-. "$HOME/.cargo/env"
-sudo apt-get install libssl-dev pkg-config -y
-cargo install cargo-watch cargo-modules cargo-nextest cargo-make cargo-release cargo-edit cargo-audit cargo-tarpaulin
+}
 
-# Install and configure pyenv
-sudo apt install libedit-dev zlib1g zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev -y
-curl https://pyenv.run | bash
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-pyenv install 2.7.18
-pyenv install 3.11.2
+Irequirements() {
+  sudo apt install build-essential dkms gcc wget make zip unzip git curl -y &&
+    echo -e "\n\nRequirements was Installed!"
+
+}
+
+Izsh() {
+  sudo apt install zsh -y
+  chsh -s /bin/zsh -y
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ &
+  {ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &
+  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions &&
+    echo -e "\n\n ZSH + Plugins & Theme was Installed!"
+}
+
+Invm() {
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash &&
+    echo -e "NVM was Installed!"
+}
+
+Ijava() {
+  curl -o- -s -fsSL "https://get.sdkman.io" | bash &&
+    echo -e "\n\nSDKMan! was Installed!"
+}
+
+Irust() {
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  . "$HOME/.cargo/env" &&
+    sudo apt-get install libssl-dev pkg-config -y &&
+    cargo install cargo-watch cargo-modules cargo-nextest cargo-make cargo-release cargo-edit cargo-audit cargo-tarpaulin &&
+    echo -e "Rust + Cargo Plugins was Installed!"
+}
+
+Ipython() {
+  sudo apt install libedit-dev zlib1g zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev -y &&
+    curl https://pyenv.run | bash &&
+    echo -e "\n\nPyEnv was Installed!"
+}
+
+Idocker() {
+  if command -v docker &>/dev/null; then
+    echo "Docker is already installed"
+  else
+    if isWSL; then
+      echo
+      echo "WSL DETECTED: We recommend using Docker Desktop for Windows."
+      echo "Please get Docker Desktop from https://www.docker.com/products/docker-desktop/"
+      echo
+
+      local choice
+
+      while true; do
+        tput cup 5 0
+        read -n 1 -p "Continue? [y/n]: " choice
+        case "$choice" in
+        [Nn])
+          echo -e "\n\nDocker Installation Canceled"
+          break
+          ;;
+        [Yy])
+          continueDocker
+          break
+          ;;
+        *)
+          tput cup 5 0
+          echo "Continue? [y/n]:  "
+          ;;
+        esac
+      done
+    fi
+    echo "Docker"
+  fi
+}
+
+continueDocker() {
+  curl -o- -fsSL https://get.docker.com | bash &
+  sudo groupadd docker &
+  sudo usermod -aG docker $USER &
+  newgrp docker &
+  sudo chown "$USER":"$USER" /home/"$USER"/.docker -R &
+  sudo chmod g+rwx "$HOME/.docker" -R &
+  sudo systemctl enable docker.service &
+  sudo systemctl enable containerd.service &&
+    echo -e "\n\nDocker wa Installed!"
+}
+
+isWSL() {
+  case "$(uname -r)" in
+  *microsoft*) true ;; # WSL 2
+  *Microsoft*) true ;; # WSL 1
+  *) false ;;
+  esac
+}
+
+main
