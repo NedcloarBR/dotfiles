8fed2db2f13ba3159ca21aa50d50e819675567c2
Miguel Alexandre Uhlein <nedcloar1@hotmail.com>
2024-11-02 14:06:55 -0300

    Update install.sh

--- Linux/install.sh
+++ Linux/install.sh
@@ -132,12 +132,12 @@ Izsh() {
   sudo apt install zsh -y
   chsh -s /bin/zsh -y
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ &
-  ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom/themes/powerlevel10k &
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &
-  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions &&
-    echo -e "\n\n ZSH + Plugins & Theme was Installed!"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k &
+  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions &
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting &
+  git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions &
+  wait
+  echo -e "\n\n ZSH + Plugins & Theme was Installed!"
 }
 
 Invm() {
